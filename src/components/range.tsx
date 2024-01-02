510b28a1792ecb14160cc3dc0d6d262815ebb4b6
danalonso18 <danaalonso1812@gmail.com>
2024-01-02 17:44:57 -0600

    Functionality

--- src/components/range.tsx
+++ src/components/range.tsx
@@ -1,21 +1,67 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 const Range: React.FC = () => {
-    const rate: number = .1398/360;
-    const [amount, setAmount] = useState<number>();
-
+    const [amount, setAmount] = useState<number>(500);
+    const [time,setTime] = useState<number>(1);
+    
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newAmount = parseInt(event.target.value)
         setAmount(newAmount) 
     }
 
-    const verificatedType = (event: React.ChangeEvent<HTMLInputElement>) => {
-        let typePerformance = document.getElementById('typePerformance');
-        const newTypePerformance = event.target.value
+    const timeChange = (event: React.ChangeEvent<HTMLSelectElement>)=>{
+        const newTime = parseInt(event.target.value)
+        console.log(newTime)
+        setTime(newTime)
+    }
+    
+    const operation = (amount:number|undefined)=>{
+        let n:number=0;
+        let r:number=0;
+        let rate: number = .1398/360;
+        for(let i=0;i<(360*time);i++){
+            if(i%30===0){
+                if(amount != undefined){
+                    n+=amount;
+                }
+            }
+            r=n*rate;
+            n=n+r
+        }
+        return n
+    }
+    
+    const opeDep = (amount: number|undefined)=>{
+        let dep:number=0;
+        if(amount != undefined){
+            dep = amount*12*time 
+        }
+        return dep
     }
 
-    return (
-        <div>
+    const accRen = ()=>{
+        const rend:number = Number(operation(amount) - opeDep(amount))
+        return rend
+    }
+
+
+    
+    useEffect(()=>{
+        console.log(time)
+    },[time])
+    
+    useEffect(()=>{
+        console.log(amount)
+    },[amount])
+    
+    
+    // const verificatedType = (event: React.ChangeEvent<HTMLInputElement>) => {
+        //     let typePerformance = document.getElementById('typePerformance');
+        //     const newTypePerformance = event.target.value
+        // }
+        
+        return (
+            <div>
             <label htmlFor="typePerformance">Selecciona el tipo de depositos que haras: </label>
             <select id="typePermorfance">
                 <option value="month">Cada mes</option>
@@ -27,19 +73,18 @@ const Range: React.FC = () => {
             <label id="min">$500</label>
             <output id="outVal" className="outVal"></output>
             <input id="range" type="range" min="500" max="23000" step="500" onChange={handleChange}/>
-            {/* onChange={document.getElementById('outVal').value = value} */}
             <label id="max">$23000</label>
             <br />
-            <label htmlFor="selectYears">Tiempo: </label>
-            <select id="selectYears">
-                <option value="one">1 año</option>
-                <option value="two">2 años</option>
-                <option value="five">5 años</option>
+            <label htmlFor="selectYears">Tiem1po: </label>
+            <select id="selectYears" onChange={timeChange} value={time}>
+                <option  value={1} >1 año</option>
+                <option value={2} >2 años</option>
+                <option  value={5} >5 años</option>
             </select>
             <br />
-            <p id="deposit">Tu deposito: ${ }</p>
-            <p id="performance">Rendimientos acumulados: ${ }</p>
-            <p id="total">Al finalizar tendras: ${ }</p>
+            <p id="deposit">Tu deposito: ${String(opeDep(amount))}</p>
+            <p id="performance">Rendimientos acumulados: ${String(accRen().toFixed(2))}</p>
+            <p id="total">Al finalizar tendras: ${String(operation(amount).toFixed(2))}</p>
         </div>
     )
 }
