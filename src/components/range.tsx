e53228f82c7cc544b400db7b558d20043df3964b
danalonso18 <danaalonso1812@gmail.com>
2024-01-03 10:06:44 -0600

    sSelect Functionality

--- src/components/range.tsx
+++ src/components/range.tsx
@@ -1,94 +1,116 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
 const Range: React.FC = () => {
-    const [amount, setAmount] = useState<number>(500);
-    const [time,setTime] = useState<number>(1);
-    
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newAmount = parseInt(event.target.value)
-        setAmount(newAmount) 
-    }
+  const [amount, setAmount] = useState<number>(500);
+  const [time, setTime] = useState<number>(1);
+  const [typeDeposit, setTypeDeposit] = useState<string>("month");
 
-    const timeChange = (event: React.ChangeEvent<HTMLSelectElement>)=>{
-        const newTime = parseInt(event.target.value)
-        console.log(newTime)
-        setTime(newTime)
-    }
-    
-    const operation = (amount:number|undefined)=>{
-        let n:number=0;
-        let r:number=0;
-        let rate: number = .1398/360;
-        for(let i=0;i<(360*time);i++){
-            if(i%30===0){
-                if(amount != undefined){
-                    n+=amount;
-                }
-            }
-            r=n*rate;
-            n=n+r
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newAmount = parseInt(event.target.value);
+    setAmount(newAmount);
+  };
+
+  const timeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newTime = parseInt(event.target.value);
+    setTime(newTime);
+  };
+
+  const typeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newType = String(event.target.value);
+    setTypeDeposit(newType);
+  };
+
+  const operation = (amount: number) => {
+    let n: number = 0;
+    let r: number = 0;
+    let result:number = 0;
+    let rate: number = 0.1398/360;
+    if (typeDeposit === "month") {
+    for (let i = 0; i < (360 * time); i++) {
+        if (i % 30 === 0) {
+          n = n + amount;
         }
-        return n
+        r = n * rate;
+        n = n + r;
+        result = n;
+      }
+    } else {
+      let onlyDeposit: number = amount;
+      for (let i=0; i<time; i++){
+        r = (onlyDeposit * 0.15); 
+        onlyDeposit = onlyDeposit + r;
+      }
+      result = onlyDeposit
     }
-    
-    const opeDep = (amount: number|undefined)=>{
-        let dep:number=0;
-        if(amount != undefined){
-            dep = amount*12*time 
-        }
-        return dep
-    }
+    return result
+  };
 
-    const accRen = ()=>{
-        const rend:number = Number(operation(amount) - opeDep(amount))
-        return rend
-    }
+  const opeDep = (amount: number) => {
+    return typeDeposit === "month" ? amount * 12 * time : amount;
+  };
 
+  const accRen = () => {
+    return Number(operation(amount) - opeDep(amount));
+  };
 
-    
-    useEffect(()=>{
-        console.log(time)
-    },[time])
-    
-    useEffect(()=>{
-        console.log(amount)
-    },[amount])
-    
-    
-    // const verificatedType = (event: React.ChangeEvent<HTMLInputElement>) => {
-        //     let typePerformance = document.getElementById('typePerformance');
-        //     const newTypePerformance = event.target.value
-        // }
-        
-        return (
-            <div>
-            <label htmlFor="typePerformance">Selecciona el tipo de depositos que haras: </label>
-            <select id="typePermorfance">
-                <option value="month">Cada mes</option>
-                <option value="only">Unica vez</option>
-            </select>
-            <br />
-            <br />
-            <label id="amount">Monto: ${amount}</label><br />
-            <label id="min">$500</label>
-            <output id="outVal" className="outVal"></output>
-            <input id="range" type="range" min="500" max="23000" step="500" onChange={handleChange}/>
-            <label id="max">$23000</label>
-            <br />
-            <label htmlFor="selectYears">Tiem1po: </label>
-            <select id="selectYears" onChange={timeChange} value={time}>
-                <option  value={1} >1 año</option>
-                <option value={2} >2 años</option>
-                <option  value={5} >5 años</option>
-            </select>
-            <br />
-            <p id="deposit">Tu deposito: ${String(opeDep(amount))}</p>
-            <p id="performance">Rendimientos acumulados: ${String(accRen().toFixed(2))}</p>
-            <p id="total">Al finalizar tendras: ${String(operation(amount).toFixed(2))}</p>
-        </div>
-    )
-}
+  useEffect(() => {
+    console.log(time);
+  }, [time]);
 
+  useEffect(() => {
+    console.log(amount);
+  }, [amount]);
 
+  useEffect(() => {
+    console.log(typeDeposit);
+  }, [typeDeposit]);
+
+  // const verificatedType = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //     let typePerformance = document.getElementById('typePerformance');
+  //     const newTypePerformance = event.target.value
+  // }
+
+  return (
+    <div>
+      <label htmlFor="typePerformance">
+        Selecciona el tipo de depositos que haras:{" "}
+      </label>
+      <select id="typePermorfance" onChange={typeChange} value={typeDeposit}>
+        <option value="month">Cada mes</option>
+        <option value="only">Unica vez</option>
+      </select>
+      <br />
+      <br />
+      <label id="amount">Monto: ${amount}</label>
+      <br />
+      <label id="min">$500</label>
+      <output id="outVal" className="outVal"></output>
+      <input
+        id="range"
+        type="range"
+        min="500"
+        max="23000"
+        step="500"
+        onChange={handleChange}
+      />
+      <label id="max">$23000</label>
+      <br />
+      <label htmlFor="selectYears">Tiem1po: </label>
+      <select id="selectYears" onChange={timeChange} value={time}>
+        <option value={1}>1 año</option>
+        <option value={2}>2 años</option>
+        <option value={5}>5 años</option>
+      </select>
+      <br />
+      <p id="deposit">Tu deposito: ${String(opeDep(amount))}</p>
+      <p id="performance">
+        Rendimientos acumulados: ${String(accRen().toFixed(2))}
+      </p>
+      <p id="total">
+        Al finalizar tendras: ${String(operation(amount)?.toFixed(2))}
+      </p>
+    </div>
+  );
+};
 
 export default Range;
